16effef9db5985271184643236a734060f5eba06
Rense Bakker <b.rense@gmail.com>
2022-04-07 20:33:16 +0200

    removed some generated code

--- src/signalingServer.ts
+++ src/signalingServer.ts
@@ -20,7 +20,7 @@ const httpServer = http.createServer(app)
 const websocket = new WebSocketServer(httpServer)
 
 
-// socket listeners https://socket.io/docs/v3/emit-cheatsheet/
+// socket listeners (https://socket.io/docs/v3/emit-cheatsheet/)
 websocket.on('connection', socket => {
   console.log(`peer #${socket.id} connected`)
   socket.broadcast.emit('signal', socket.id)
@@ -28,6 +28,7 @@ websocket.on('connection', socket => {
   socket.on('offer', payload => socket.broadcast.emit('offer', payload))
   socket.on('answer', payload => socket.broadcast.emit('answer', payload))
   socket.on('candidate', payload => socket.broadcast.emit('candidate', payload))
+
   socket.on('disconnecting', () => socket.broadcast.emit('disconnecting', socket.id))
   socket.on('disconnect', () => {
     socket.broadcast.emit('disconnected', socket.id)
