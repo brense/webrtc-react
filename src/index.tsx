9174c2ec7bd91d65828e80d9a2aa899a7a9deb23
Rense Bakker <b.rense@gmail.com>
2022-04-08 08:52:31 +0200

    implement webrtc client

--- src/index.tsx
+++ src/index.tsx
@@ -4,38 +4,20 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import io from 'socket.io-client'
-import createWebRTCClient from './WebRTCClient'
-import { Observable } from 'rxjs'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import { WebRTCIOClientProvider } from '.'
 
 const port = window.location.port || (window.location.protocol === 'https:' ? 443 : 80)
 const socketUrl = `${window.location.protocol}://${window.location.hostname}:${port}`
 const socket = io(socketUrl)
 
-const webRTCClient = createWebRTCClient({
-  onConnect: () => ({
-    onSignal: new Observable(subscriber => { socket.on('signal', subscriber.next) }),
-    onOffer: new Observable(subscriber => { socket.on('offer', subscriber.next) }),
-    onAnswer: new Observable(subscriber => { socket.on('answer', subscriber.next) }),
-    onCandidate: new Observable(subscriber => { socket.on('candidate', subscriber.next) })
-  }),
-  sendOffer: offer => socket.emit('offer', offer),
-  sendAnswer: answer => socket.emit('answer', answer),
-  sendCandidate: candidate => socket.emit('candidate', candidate)
-})
-
-socket.on('connect', () => {
-  console.log(`Connected to websocket, id: ${socket.id}`)
-  webRTCClient.connect(() => socket.id)
-})
-
-webRTCClient.onMessage(message => console.log(message))
+ReactDOM.render(
+  <React.StrictMode>
+    <WebRTCIOClientProvider socket={socket}>
+      <App />
+    </WebRTCIOClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
